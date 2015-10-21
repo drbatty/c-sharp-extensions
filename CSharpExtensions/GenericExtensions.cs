7e5b13e93d06940381f895f70be9addae28c1f1e
Michael Batty <batty.mathmo@googlemail.com>
2015-10-21 08:36:58 +0100

    added more generic extensions

--- CSharpExtensions/GenericExtensions.cs
+++ CSharpExtensions/GenericExtensions.cs
@@ -1,10 +1,102 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
 
 namespace CSharpExtensions
 {
     public static class GenericExtensions
     {
+        #region containment
+
+        /// <summary>
+        /// Extension which returns true if the supplied generic object is equal to at least one of the supplied parameters
+        /// (syntactic sugar)
+        /// </summary>
+        /// <typeparam name="T">generic type to test equality on</typeparam>
+        /// <param name="t">The generic object for equality testing</param>
+        /// <param name="values"> Zero or more objects of the supplied generic type, for comparision</param>
+        /// <returns> true if and only if the supplied generic object is equal to at least one of the supplied parameters </returns>
+        public static bool In<T>(this T t, params T[] values)
+        {
+            return values.Any(x => t.Equals(x));
+        }
+
+        /// <summary>
+        /// Extension which returns true if the supplied generic object is not equal to any of the supplied parameters
+        /// (syntactic sugar)
+        /// </summary>
+        /// <typeparam name="T">generic type to test equality on</typeparam>
+        /// <param name="t">The generic object for equality testing</param>
+        /// <param name="values"> Zero or more objects of the supplied generic type, for comparision</param>
+        /// <returns> true if and only if the supplied generic object is not equal to any of the supplied parameters </returns>
+
+        public static bool NotIn<T>(this T t, params T[] values)
+        {
+            return !t.In(values);
+        }
+
+        public static T Clamp<T>(this T source, T start, T end) where T : IComparable //50.Clamp( 0, 20 ); // = 20
+        {
+            var isReversed = start.CompareTo(end) > 0;
+            var smallest = isReversed ? end : start;
+            var biggest = isReversed ? start : end;
+
+            return source.CompareTo(smallest) < 0
+                ? smallest
+                : source.CompareTo(biggest) > 0
+                    ? biggest
+                    : source;
+        }
+
+        public static bool Between<T>(this T value, T from, T to) where T : IComparable<T>
+        {
+            return value.CompareTo(@from) >= 0 && value.CompareTo(to) <= 0;
+        }
+
+        #endregion
+
+        #region chaining
+
+        /// <summary>
+        /// Extension which applies an action to a parameter t then returns the parameter, thus stopping the parameter being "swallowed up" by the action.
+        /// Can be used for tidying code, such as in tests, by reducing two or three lines of code to one. 
+        /// Debatable whether it improves readability ...
+        /// </summary>
+        /// <typeparam name="T">the parameter type for the action</typeparam>
+        /// <param name="t">The parameter for the action</param>
+        /// <param name="λ"> The action to apply to the parameter </param>
+        /// <returns> the supplied parameter </returns>
+        public static T Return<T>(this T t, Action<T> λ)
+        {
+            λ(t);
+            return t;
+        }
+
+        #endregion
+
+        #region conversion
+
+        public static List<T> WrapInList<T>(this T item)
+        {
+            return new List<T> { item };
+        }
+
+        public static string ToXml<T>(this T obj) where T : class
+        {
+            var s = new XmlSerializer(obj.GetType());
+            using (var writer = new StringWriter())
+            {
+                s.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+
+        #endregion
+
+        #region tuple handling
+
         /// <summary>
         /// Extension which returns a pair of generic objects by combining 
         /// the given object with another supplied object. Saves having to write
@@ -20,9 +112,44 @@ namespace CSharpExtensions
             return new Tuple<T1, T2>(t1, t2);
         }
 
-        public static List<T> WrapInList<T>(this T item)
+        /// <summary>
+        /// Extension which returns a triple of generic objects by combining 
+        /// the given object with two other supplied objects. Saves having to write
+        /// new Tuple ...
+        /// </summary>
+        /// <typeparam name="T1">the type of the first object in the triple</typeparam>
+        /// <typeparam name="T2">the type of the second object in the triple</typeparam>
+        /// <typeparam name="T3">the type of the third object in the triple</typeparam>
+        /// <param name="t1">the first object in the triple</param>
+        /// <param name="t2">the second object in the triple </param>
+        /// <param name="t3">the third object in the triple </param>
+        /// <returns> a tuple of three objects (triple) containing all of the supplied objects </returns>
+        public static Tuple<T1, T2, T3> Triple<T1, T2, T3>(this T1 t1, T2 t2, T3 t3)
         {
-            return new List<T> { item };
+            return new Tuple<T1, T2, T3>(t1, t2, t3);
         }
+
+        #endregion
+
+        #region null handling
+
+        public static void ReturningIfNull<T, TArg>(this TArg t, T s, Action<TArg> λ)
+        {
+            // ReSharper disable once CompareNonConstrainedGenericWithNull
+            if (s != null)
+                λ(t);
+        }
+
+        public static TResult DefaultIfNull<TArg, TResult>(this TArg t, Func<TArg, TResult> λ) where TArg : class
+        {
+            return t == null ? default(TResult) : λ(t);
+        }
+
+        public static string ToStringOrEmpty<T>(this T t) where T : class
+        {
+            return DefaultIfNull(t, o => o.ToString());
+        }
+
+        #endregion
     }
 }
--- CSharpExtensionsTests/GenericExtensionsTests.cs
+++ CSharpExtensionsTests/GenericExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using CSharpExtensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -6,6 +7,120 @@ namespace CSharpExtensionsTests
     [TestClass]
     public class GenericExtensionsTests
     {
+        #region containment
+
+        #region in tests
+
+        [TestMethod]
+        public void InTestTrue()
+        {
+            "a".In("a", "b", "c").ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void InTestFalse()
+        {
+            "a".In("z", "b", "c").ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void InTestNull()
+        {
+            "a".In(null, "b", "c").ShouldBeFalse();
+        }
+
+        #endregion
+
+        #region notin tests
+
+        [TestMethod]
+        public void NotInTestTrue()
+        {
+            "a".NotIn("b", "c", "d").ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void NotInTestFalse()
+        {
+            "a".NotIn("a", "b").ShouldBeFalse();
+        }
+
+        #endregion
+
+        #region clamp tests
+
+        [TestMethod]
+        public void ClampTestHigher()
+        {
+            50.Clamp(0, 20).ShouldEqual(20);
+        }
+
+        [TestMethod]
+        public void ClampTestLower()
+        {
+            (-50).Clamp(0, 20).ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void ClampTestReversed()
+        {
+            50.Clamp(20, 0).ShouldEqual(20);
+        }
+
+        #endregion
+
+        #region between tests
+
+        [TestMethod]
+        public void BetweenTestTrue()
+        {
+            var between = new DateTime(2021, 1, 1);
+            var from = new DateTime(1941, 3, 1);
+            var to = new DateTime(2031, 12, 20);
+
+            between.Between(from, to).ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void BetweenTestFalse1()
+        {
+            var between = new DateTime(2121, 1, 1);
+            var from = new DateTime(1941, 3, 1);
+            var to = new DateTime(2031, 12, 20);
+
+            between.Between(from, to).ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void BetweenTestFalse2()
+        {
+            var between = new DateTime(1821, 1, 1);
+            var from = new DateTime(1941, 3, 1);
+            var to = new DateTime(2031, 12, 20);
+
+            between.Between(from, to).ShouldBeFalse();
+        }
+
+        #endregion
+
+        #endregion
+
+        #region chaining
+
+        [TestMethod]
+        public void ReturnActionTest()
+        {
+            var total = 0;
+
+            1.Return(a => total += a).ShouldEqual(1);
+        }
+
+        #endregion
+
+        #region tuple handling
+
+        #region pair tests
+
         [TestMethod]
         public void PairTest1()
         {
@@ -24,6 +139,40 @@ namespace CSharpExtensionsTests
             "1".Pair<string, string>(null);
         }
 
+        #endregion
+
+        #region triple tests
+
+        [TestMethod]
+        public void TripleTest1()
+        {
+            "1".Triple(2, "3").Item1.ShouldEqual("1");
+        }
+
+        [TestMethod]
+        public void TripleTest2()
+        {
+            "1".Triple(2, "3").Item2.ShouldEqual(2);
+        }
+
+        [TestMethod]
+        public void TripleTest3()
+        {
+            "1".Triple(2, "3").Item3.ShouldEqual("3");
+        }
+
+        [TestMethod]
+        public void TripleTestNull()
+        {
+            "1".Triple<string, string, string>(null, null);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region conversion tests
+
         [TestMethod]
         public void WrapInListTest()
         {
@@ -31,5 +180,38 @@ namespace CSharpExtensionsTests
             list.ShouldNumber(1);
             list.ShouldContain(1);
         }
+
+        #endregion
+
+        #region null handling tests
+
+        [TestMethod]
+        public void ReturningIfNullTestValueType()
+        {
+            var total = 0;
+            Action<int> addOne = n => total += 1;
+            1.ReturningIfNull(2, addOne);
+            total.ShouldEqual(1);
+        }
+
+        [TestMethod]
+        public void ReturningIfNullTestNull()
+        {
+            var total = 0;
+            Action<int> addOne = n => total += 1;
+            1.ReturningIfNull<string, int>(null, addOne);
+            total.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void ReturningIfNullTestNotNull()
+        {
+            var total = 0;
+            Action<string> addOne = @string => total += 1;
+            "a".ReturningIfNull("b", addOne);
+            total.ShouldEqual(1);
+        }
+
+        #endregion
     }
 }
