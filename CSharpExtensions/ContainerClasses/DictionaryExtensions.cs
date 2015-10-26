bbb11b3821ea73de725d27c434092bd23c290579
Michael Batty <batty.mathmo@googlemail.com>
2015-10-26 09:29:01 +0000

    add more dictionary extensions

--- CSharpExtensions/ContainerClasses/DictionaryExtensions.cs
+++ CSharpExtensions/ContainerClasses/DictionaryExtensions.cs
@@ -91,6 +91,120 @@ namespace CSharpExtensions.ContainerClasses
 
         #endregion
 
+        #region insertion
+
+        /// <summary>
+        /// Sets the given key of a dictionary to a given value, provided that neither key nor value are null  
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary's keys</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary's values</typeparam>
+        /// <param name="dictionary"> The dictionary whose key/value pair to set</param>
+        /// <param name="key">The key to set</param>
+        /// <param name="value">The value to set the key to</param>
+        public static void SetIfNotNull<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key,
+            TValue value)
+        {
+            // ReSharper disable CompareNonConstrainedGenericWithNull
+            if (key == null || value == null)
+                // ReSharper restore CompareNonConstrainedGenericWithNull
+                return;
+
+            dictionary[key] = value;
+        }
+
+        public static void Set<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            IDictionary<TKey, TValue> setValues)
+        {
+            setValues.EachKey(key => dictionary[key] = setValues[key]);
+        }
+
+        /// <summary>
+        /// Returns an existing value U for key T, or creates a new instance of type U using the default constructor, 
+        /// adds it to the dictionary and returns it, e.g. eg return _sessionDic.GetOrInsertNew(sessionId);
+        /// </summary>
+        /// <typeparam name="TKey">the type of the dictionary's keys</typeparam>
+        /// <typeparam name="TValue">the type of the dictionary's values</typeparam>
+        /// <param name="dictionary">the dictionary</param>
+        /// <param name="key">the key</param>
+        /// <returns></returns>
+        public static TValue Upsert<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : new()
+        {
+            if (dictionary.ContainsKey(key)) return dictionary[key];
+            var newObj = new TValue();
+            dictionary[key] = newObj;
+            return newObj;
+        }
+
+        #endregion
+
+        #region conversion
+
+        public static Dictionary<string, string> ToStrings<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            var result = new Dictionary<string, string>();
+            dictionary.EachKey(key => result[key.ToString()] = dictionary[key].ToString());
+            return result;
+        }
+
+        #endregion
+
+        #region containment
+
+        /// <summary>
+        /// Extension that returns true if a dictionary contains the keys supplied as parameters 
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary's keys</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary's values</typeparam>
+        /// <param name="dictionary"> The dictionary whose keys to test</param>
+        /// <param name="keys"> Zero or more keys to look for</param>
+        /// <returns> true if and only if the given dictionary contains the keys supplied as parameters</returns>
+        public static bool ContainsKeys<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, params TKey[] keys)
+        {
+            return keys.NotDefault().All(dictionary.ContainsKey);
+        }
+
+        /// <summary>
+        /// Extension that returns true if a dictionary contains the values supplied as parameters 
+        /// </summary>
+        /// <typeparam name="TKey">The type of the dictionary's keys</typeparam>
+        /// <typeparam name="TValue">The type of the dictionary's values</typeparam>
+        /// <param name="dictionary"> The dictionary whose values to test</param>
+        /// <param name="values"> Zero or more values to look for</param>
+        /// <returns> true if and only if the given dictionary contains the values supplied as parameters</returns>
+        public static bool ContainsValues<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, params TValue[] values)
+        {
+            return values.NotDefault().All(dictionary.ContainsValue);
+        }
+
+        #endregion
+
+        #region lookup
+
+        /// <summary>
+        /// either gets the value for a key or returns default if the dictionary does not have the key
+        /// </summary>
+        /// <typeparam name="TKey">the type of the key</typeparam>
+        /// <typeparam name="TValue">the type of the value</typeparam>
+        /// <param name="dictionary">the dictionary to retrieve the value from</param>
+        /// <param name="key">the key of the value to retrieve</param>
+        /// <returns>the value for a key, or default if the dictionary does not have the key</returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.ContainsKey(key) ? dictionary[key] : default(TValue);
+        }
+
+        #endregion
+
+        #region counting
+
+        public static int CountKeys<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Func<TKey, bool> π)
+        {
+            return dictionary.Keys.Count(π);
+        }
+
+        #endregion
+
         #region restriction, image and preimage
 
         public static IDictionary<TKey, TValue> Restrict<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
@@ -114,6 +228,29 @@ namespace CSharpExtensions.ContainerClasses
             Set<TValue> values)
         {
             return dictionary.Restrict(dictionary.Preimage(values));
+        }
+
+        #endregion
+
+        #region filtering
+
+        public static IDictionary<TKey, TValue> WhereKeys<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Func<TKey, bool> π)
+        {
+            return dictionary.Restrict(dictionary.Keys.Where(π).ToSet());
+        }
+
+        public static IDictionary<TKey, TValue> WhereValues<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Func<TValue, bool> π)
+        {
+            return dictionary.Restrict(dictionary.Values.Where(π).ToSet());
+        }
+
+        #endregion
+
+        #region formatting
+
+        public static string Inspect<TKey, TValue>(this IDictionary<TKey, TValue> dictionary)
+        {
+            return "{" + string.Join(", ", dictionary.Keys.Select(key => key + "->" + dictionary[key])) + "}";
         }
 
         #endregion
--- CSharpExtensionsTests/ContainerClasses/DictionaryExtensionsTests.cs
+++ CSharpExtensionsTests/ContainerClasses/DictionaryExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using CSharpExtensions.ContainerClasses;
 using CSharpExtensions.Mathematical.Sets;
+using CSharpExtensionsTests.GeneralFixtures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSharpExtensionsTests.ContainerClasses
@@ -8,13 +9,20 @@ namespace CSharpExtensionsTests.ContainerClasses
     [TestClass]
     public class DictionaryExtensionsTests
     {
-        #region fixture
+        #region fixtures
 
         private static readonly Dictionary<string, int> Dict = new Dictionary<string, int>
         {
             {"a", 1},
             {"b", 7},
             {"c", 9}
+        };
+
+        public static Dictionary<string, string> SimpleDict = new Dictionary<string, string>
+        {
+            {"a", "x"},
+            {"b", "y"},
+            {"c", "z"}
         };
 
         #endregion
@@ -89,6 +97,162 @@ namespace CSharpExtensionsTests.ContainerClasses
 
         #endregion
 
+        #region insertion
+
+        [TestMethod]
+        public void SetTest()
+        {
+            var initialDict = new Dictionary<string, int>
+                {
+                    {"a", 0},
+                    {"b", 0},
+                    {"c", 0}
+                };
+            var newDict = new Dictionary<string, int>
+                {
+                    {"a", 2},
+                    {"b", 3},
+                    {"c", 4}
+                };
+            initialDict.Set(newDict);
+            initialDict["a"].ShouldEqual(2);
+            initialDict["b"].ShouldEqual(3);
+            initialDict["c"].ShouldEqual(4);
+        }
+
+        [TestMethod]
+        public void SetIfNotNullTestFirstNull()
+        {
+            var dict = new Dictionary<string, int>();
+            dict.SetIfNotNull(null, 1);
+
+            dict.Keys.ShouldNumber(0);
+        }
+
+        [TestMethod]
+        public void SetIfNotNullTestSecondNull()
+        {
+            var dict = new Dictionary<string, string>();
+            dict.SetIfNotNull("a", null);
+
+            dict.Keys.ShouldNumber(0);
+        }
+
+        [TestMethod]
+        public void SetIfNotNullTest()
+        {
+            var dict = new Dictionary<string, string>();
+            dict.SetIfNotNull("a", "b");
+
+            dict.Keys.ShouldNumber(1);
+        }
+
+        #endregion
+
+        #region conversion
+
+        [TestMethod]
+        public void ToStringsTest()
+        {
+            var toStrings = Dict.ToStrings();
+            toStrings["a"].ShouldEqual("1");
+            toStrings["b"].ShouldEqual("7");
+            toStrings["c"].ShouldEqual("9");
+        }
+
+        #endregion
+
+        #region containment
+
+        [TestMethod]
+        public void ContainsKeysTestTrue()
+        {
+            SimpleDict.ContainsKeys("b", "c").ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsKeysTestFalse()
+        {
+            SimpleDict.ContainsKeys("b", "c", "z").ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void ContainsKeysTestNull()
+        {
+            SimpleDict.ContainsKeys(null, null, null).ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsValuesTestTrue()
+        {
+            SimpleDict.ContainsValues("x", "z").ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ContainsValuesTestFalse()
+        {
+            SimpleDict.ContainsValues("b", "x", "z").ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void ContainsValuesTestNull()
+        {
+            SimpleDict.ContainsValues(null, null, null).ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void GetOrDefaultTestNull()
+        {
+            Assert.IsNull(new Dictionary<string, string>().GetOrDefault("a"));
+        }
+
+        #endregion
+
+        #region lookup
+
+        [TestMethod]
+        public void GetOrDefaultTestNonNull()
+        {
+            new Dictionary<string, string> { { "a", "b" } }.GetOrDefault("a").ShouldEqual("b");
+        }
+
+        [TestMethod]
+        public void GetOrInsertNewTestDoesntExist()
+        {
+            var dict = new Dictionary<string, Person>();
+            dict.Upsert("michael");
+            dict.ContainsKey("michael").ShouldBeTrue();
+            Assert.IsNull(dict["michael"].Name);
+            dict["michael"].Age.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void GetOrInsertNewTestExists()
+        {
+            var dict = new Dictionary<string, Person> { { "michael", new Person { Name = "Michael", Age = 43 } } };
+            dict.Upsert("michael");
+            dict["michael"].Name.ShouldEqual("Michael");
+            dict["michael"].Age.ShouldEqual(43); // fail. it's 44 now
+        }
+
+        #endregion
+
+        #region counting
+
+        [TestMethod]
+        public void CountKeysTest()
+        {
+            var dict = new Dictionary<string, int>
+                {
+                    {"a", 2},
+                    {"b", 3},
+                    {"cd", 4}
+                };
+            dict.CountKeys(k => k.Length == 1).ShouldEqual(2);
+        }
+
+        #endregion
+
         #region restriction, image and preimage
 
         [TestMethod]
@@ -127,6 +291,46 @@ namespace CSharpExtensionsTests.ContainerClasses
             Dict.Restrict(Dict.Values.ToSet()).ShouldBeEquivalentMappingTo(Dict);
             Dict.Restrict(new Set<int>()).ShouldBeEquivalentMappingTo(new Dictionary<string, int>());
             Dict.Restrict(new Set<int> { 7 }).ShouldBeEquivalentMappingTo(new Dictionary<string, int> { { "b", 7 } });
+        }
+
+        #endregion
+
+        #region filtering
+
+        [TestMethod]
+        public void WhereKeysTest()
+        {
+            Dict.WhereKeys(key => key == "a" || key == "c")
+                .ShouldBeEquivalentMappingTo(new Dictionary<string, int>
+                    {
+                        {"a", 1}, {"c", 9}
+                    });
+        }
+
+        [TestMethod]
+        public void WhereValuesTest()
+        {
+            Dict.WhereValues(value => value > 1)
+                .ShouldBeEquivalentMappingTo(new Dictionary<string, int>
+                    {
+                        {"b", 7}, {"c", 9}
+                    });
+        }
+
+        #endregion
+
+        #region formatting
+
+        [TestMethod]
+        public void InspectTest()
+        {
+            Dict.Inspect().ShouldEqual("{a->1, b->7, c->9}");
+        }
+
+        [TestMethod]
+        public void InspectTestEmpty()
+        {
+            new Dictionary<string, int>().Inspect().ShouldEqual("{}");
         }
 
         #endregion
